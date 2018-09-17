e1d09ffe2c6ee5e21dd9db52afbc52b1e2f7f143
James MacDonald <jamesdavidmacdonald@gmail.com>
2018-09-17 16:28:45 -0400

    working on js for my Works

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,2 +1,21 @@
-const hello = 'World';
-console.log(hello);
+const portfolio = {};
+
+portfolio.myWorks = () => {
+        let btns = document.getElementsByClassName("projectNode");
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function () {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                this.className += " active";
+            });
+    }
+}
+
+
+portfolio.init = () => {
+    portfolio.myWorks();
+}
+
+$(function () {
+    portfolio.init();
+});
