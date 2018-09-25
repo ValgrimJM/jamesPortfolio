b66ef819507443c598e5ab9621d0caa9fce73812
James MacDonald <jamesdavidmacdonald@gmail.com>
2018-09-25 18:46:00 -0400

    hamburger menu

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -22,9 +22,48 @@ portfolio.toTop = () => {
     $(".topButton").on("click", function () {
         console.log('Test');
         console.log($(".topButton").scrollTop());
-        document.querySelector(".bodyWrapper").scrollIntoView({
-            behavior: 'smooth' 
-        }, 1200);
+        $("html,body").animate({ scrollTop: 0 }, 700);
+        // document.querySelector(".bodyWrapper").scrollIntoView({
+        //     behavior: 'smooth' 
+        // }, 1200);
+    });
+}
+portfolio.openMenu = () => {
+    $(".menuIcon").on("click", function (){
+        $(".menuIcon").toggleClass("active");
+        $(".headerLinks").toggleClass("active")
+    });
+    $(".hamburger").on("click", function (){
+        $(".hamburger").toggleClass("is-active");
+        $(".headerLinks").toggleClass("active")
+    });
+
+}
+portfolio.scrollButtons = () => {
+    $(".aboutMe").on("click", function () {
+        console.log($(".about").position());
+        $("html,body").animate({ scrollTop: $(".about").position().top }, 700);
+        // document.querySelector(".about").scrollIntoView({
+        //     behavior: 'smooth'
+        // }, 1200);
+    });
+    $(".skills").on("click", function () {
+        $("html,body").animate({ scrollTop: $(".mySkills").position().top }, 700);
+        // document.querySelector(".mySkills").scrollIntoView({
+        //     behavior: 'smooth'
+        // }, 1200);
+    });
+    $(".works").on("click", function () {
+        $("html,body").animate({ scrollTop: $(".myWorks").position().top }, 700);
+        // document.querySelector(".myWorks").scrollIntoView({
+        //     behavior: 'smooth'
+        // }, 1200);
+    });
+    $(".contactMe").on("click", function () {
+        $("html,body").animate({ scrollTop: $(".contact").position().top }, 700);
+        // document.querySelector(".contact").scrollIntoView({
+        //     behavior: 'smooth',
+        // }, 1200);
     });
 }
 
@@ -32,6 +71,8 @@ portfolio.toTop = () => {
 portfolio.init = () => {
     portfolio.myWorks();
     portfolio.toTop();
+    portfolio.openMenu();
+    portfolio.scrollButtons();
 }
 
 $(function () {
